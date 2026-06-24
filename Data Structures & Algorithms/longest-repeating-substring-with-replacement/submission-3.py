1a08374e65999b87942c52f5667841081cbc375f
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-06-24 22:54:29 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        def isValid(x):
+            dic = Counter(x)
+            if len(dic) == 1: return True
+            if k >= (len(x) - max(dic.values())): return True
+            return False
+
+        ans = float('-inf')
+        L = 0
+
+        for R in range(len(s)):
+            if isValid(s[L:R+1]):
+                ans = max(ans, R - L + 1)
+        
+        return 0 if ans == float('-inf') else ans
