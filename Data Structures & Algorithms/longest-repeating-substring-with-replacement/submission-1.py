8dc42b624aaf2bab2599df7d932547d016a49b13
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-06-24 22:45:42 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        L = 0
+        temp = 0
+        ans = float("-inf")
+        for x in range(len(s)):
+            L = x
+            for R in range(len(s)):
+                if L != R and s[L] != s[R]:
+                    if temp == k:
+                        L = R
+                        temp = 0
+                    else:
+                        temp += 1
+                ans = max(ans, R - L + 1)
+
+        return 0 if ans == float('inf') else ans
