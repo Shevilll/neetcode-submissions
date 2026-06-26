c2b6c7b583324a025c51c63e9f5a3675107fdfc2
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-06-27 01:34:27 +0530

    Add: search-for-word-ii - submission-6

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-6.py
@@ -0,0 +1,62 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+    
+        return curr.word
+
+    def insert(self, word):
+        curr = self.root
+        
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.word = word
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        ans = []
+        root = Trie()
+        memory = set()
+        for word in words:
+            root.insert(word)
+            
+        def dfs(i, j, curr):
+            if (i, j) in memory or i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] not in curr.children:
+                return
+            
+            memory.add((i, j))
+
+            next_child = curr.children[board[i][j]]
+            if next_child.word:
+                ans.append(next_child.word)
+                next_child.word = None
+
+            dfs(i - 1, j, next_child)
+            dfs(i + 1, j, next_child)
+            dfs(i, j - 1, next_child)
+            dfs(i, j + 1, next_child)
+
+            memory.remove((i, j))
+            if not next_child.children:
+                del curr.children[board[i][j]]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                dfs(i, j, root.root)
+        
+        return ans
