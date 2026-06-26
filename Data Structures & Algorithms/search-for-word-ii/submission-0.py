ae8f8918c03e2e08585a4a02a7fe52be935eb83b
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-06-27 00:56:16 +0530

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        memory = set()
+        def dfs(i, j, c, word):
+            if c == len(word):
+                return True
+            if (i, j) in memory or i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] != word[c]:
+                return False
+            
+            memory.add((i, j))
+
+            found = dfs(i - 1, j, c + 1, word) or dfs(i + 1, j, c + 1, word) or dfs(i, j - 1, c + 1, word) or dfs(i, j + 1, c + 1, word)
+
+            memory.remove((i, j))
+            return found
+
+        ans = []
+        for word in words:
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    if dfs(i, j, 0, word):
+                        ans.append(word)
+        
+        return ans
