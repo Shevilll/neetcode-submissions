26da730bb97063531a233a9f76e6787049e6f5fc
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-06-30 23:38:52 +0530

    Add: course-schedule-iv - submission-7

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        if not prerequisites:
+            return [False] * len(queries)
+        def dfs(i, src):
+            if i in seen:
+                return
+            seen.add(i)
+            
+            if i != src:
+                visit[src].add(i)
+
+
+            for nei in adj[i]:
+                dfs(nei, src)
+
+
+        adj = {i:[] for i in range(numCourses)}
+
+        for u, v in prerequisites:
+            adj[u].append(v)
+
+        visit = defaultdict(set)
+
+        for i in range(numCourses):
+            seen = set()
+            dfs(i, i)
+
+        ans = []
+
+        for u, v in queries:
+            ans.append(v in visit[u])
+        return ans
