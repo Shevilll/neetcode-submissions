f40c8b926a04c2cc52fd95b23cb941b8565dc660
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-07-07 20:03:12 +0530

    Add: unboundedKnapsack - submission-1

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-1.py
@@ -0,0 +1,19 @@
+from functools import cache
+
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        @cache
+        def dfs(i, c):
+            if i == len(profit): return 0
+
+            maxProfit = dfs(i+1, c)
+
+            newCapacity = c - weight[i]
+            if newCapacity >= 0:
+                p = profit[i] + dfs(i, newCapacity)
+                maxProfit = max(maxProfit, p)
+
+            return maxProfit
+
+        return dfs(0, capacity)
+            
