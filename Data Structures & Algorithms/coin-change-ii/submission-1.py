04fee4358f2c2dc3ac2677029613f06c88780376
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-07-08 00:04:32 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,22 @@
+from functools import cache
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        ans = 0
+        
+        @cache
+        def dfs(i, count):
+            nonlocal ans
+            if i == len(coins) or count > amount:
+                return
+            
+            if count == amount:
+                ans += 1
+                return
+
+            dfs(i, count + coins[i])
+            dfs(i + 1, count)
+
+
+        dfs(0, 0)
+        return ans
