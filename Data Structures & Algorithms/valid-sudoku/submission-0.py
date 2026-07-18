1e032279ac59cc59cbb2f3416f44269fdc73e9b0
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-07-18 14:04:27 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool: 
+        for r in range(9):
+            rowSeen = set()
+            colSeen = set()
+            for c in range(9):
+                if board[r][c] != '.' and board[r][c] in rowSeen: return False
+                elif board[r][c] != '.': rowSeen.add(board[r][c])
+                if board[c][r] != '.' and board[c][r] in colSeen: return False
+                elif board[c][r] != '.': colSeen.add(board[c][r])
+
+        for k in range(0, 9, 3):
+            seen = set()
+            for r in range(3):
+                for c in range(3):
+                    if board[k + r][k + c] != '.' and board[k + r][k + c] in seen: return False
+                    elif board[k + r][k + c] != '.': seen.add(board[k + r][k + c])
+            print(seen)
+        return True
