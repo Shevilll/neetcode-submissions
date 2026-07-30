65bcead01f80187a0c3ed6d3a8e70360125386c0
Ahmad Faraz <115057138+Shevilll@users.noreply.github.com>
2026-07-30 22:21:06 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+
+        for i in range(n):
+            c = a + b
+            a = b
+            b = c
+
+        return b
